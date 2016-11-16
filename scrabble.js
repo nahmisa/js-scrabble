12a092969adb21144c1e209a97c777f2ba52e9bb
Sarah Nahmias <nahmisa@gmail.com>
2016-11-16 15:47:44 -0800

    Bones of tilebag...

--- scrabble.js
+++ scrabble.js
@@ -5,13 +5,13 @@ var Scrabble = function() {
   "q": 10, "r": 1, "s": 1, "t": 1, "u": 1, "v": 4, "w": 4, "x": 8, "y": 4,
   "z": 10 };
 
-  this.bonus_length = 7;
+  this.maxLength = 7;
   this.bonus = 50;
   this.tileBag = new TileBag();
 };
 
 var TileBag = function() {
-  this.default_tiles = ["a", "a", "a", "a", "a", "a", "a", "a",
+  this.defaultTiles = ["a", "a", "a", "a", "a", "a", "a", "a",
         "a", "n", "n", "n", "n", "n", "n", "b", "b", "o", "o", "o", "o", "o",
         "o", "o", "o", "c", "c", "p", "p", "d", "d", "d", "d", "q", "e", "e",
         "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "r", "r", "r", "r",
@@ -20,6 +20,28 @@ var TileBag = function() {
         "i", "i", "i", "i", "v", "v", "j", "w", "w", "k", "x", "l", "l", "l",
         "l", "y", "y", "m", "m", "z"];
 };
+
+TileBag.prototype.drawTiles = function(numOfTiles) {
+  this.randomTiles = [];
+  for(var i=0; i<numOfTiles; i++) {
+  // tiles are formatted like: [number, original index]
+    var tile = sample(this.defaultTiles);
+  // get random tiles from the bag
+    this.randomTiles.push(tile);
+  // remove those tiles from the bag so they cannot be drawn again
+  // the index 1 of tile is the random index that cause it to be drawn...
+  // this is the index in defaultTiles
+    this.defaultTiles.splice(tile[1],1);
+  }
+  return this.randomTiles;
+
+};
+
+// helper function to wrap ugly random
+function sample(array) {
+  var index = Math.floor ( Math.random() * array.length );
+  return [array[index], index];
+}
 
 Scrabble.prototype.score = function(word) {
   this.word = word.toLowerCase(); // lowercase input for comparing
@@ -32,7 +54,7 @@ Scrabble.prototype.score = function(word) {
         score += this.letterScore[this.word[i]];
       }
 
-      if (this.word.length >= this.bonus_length) {
+      if (this.word.length >= this.maxLength) {
         score += this.bonus;
       }
 
@@ -63,12 +85,14 @@ Scrabble.prototype.highestScore = function(wordArr) {
   return highScoreWord;
 };
 
-// pass two arguments, name & scrabble so that we can have multiplayer games
-var Player = function(name, scrabble = (new Scrabble())) {
+// pass two arguments, name & game3 so that we can have multiplayer games
+var Player = function(name, game = (new Scrabble())) {
   this.name = name;
   this.plays = [];
   // Each player will have their own Scrabble
-  this.scrabble = scrabble;
+  this.scrabble = game;
+  // a new player has the maximum number of tiles available to them
+  this.tiles = this.scrabble.tileBag.drawTiles(this.scrabble.maxLength);
 };
 
 Player.prototype.play = function(word) {
@@ -77,7 +101,39 @@ Player.prototype.play = function(word) {
   }
   this.word = word;
   this.plays.push(word);
+
+  this.removeTiles(word);
+  this.drawTiles(word);
+
   return this.scrabble.score(word);
+};
+
+Player.prototype.removeTiles = function(word) {
+  this.word = word;
+  for(var i=0; i<this.word; i++) {
+    var letter = word[i];
+  // remove those tiles from the player's tiles so they cannot be played again
+  // I think we will have to loop through all the player's tiles to find
+  // the one that matches a partiular letter.
+  // This is less than ideal, but because things keep getting reshuffled and
+  // we don't have any information from the letter about where it is in the arr.
+  // this is an okay solution for now :(
+    for(var j=0; j<this.tiles.length; j++) {
+      if (letter == this.tiles[j][0]) {
+        this.tiles.splice(j,1);
+        break;
+      }
+    }
+  }
+  return this.tiles;
+};
+
+Player.prototype.drawTiles = function() {
+  var tilesDrawn = this.scrabble.tileBag.drawTiles(this.scrabble.maxLength - this.tiles.length);
+  for(var i=0; i<tilesDrawn; i++) {
+    this.tiles.push(tilesDrawn[i]);
+  }
+  return this.tiles;
 };
 
 Player.prototype.totalScore = function() {
@@ -106,32 +162,5 @@ Player.prototype.highestScoringWord = function() {
 Player.prototype.highestWordScore = function() {
   return this.scrabble.score(this.highestScoringWord());
 };
-
-
-
-
-// var sarah = new Player("Sarah");
-// console.log(sarah.play("cat"));
-// console.log(sarah.play("dog"));
-// console.log(sarah.plays);
-// console.log(sarah.totalScore());
-// console.log(sarah.play("dogssss"));
-// console.log(sarah.play("dogssss"));
-// console.log(sarah.totalScore());
-// console.log(sarah.highestWordScore());
-// console.log(sarah.highestScoringWord());
-// console.log(sarah.play("frog"));
-
-
-// var scrabble = new Scrabble();
-// console.log(scrabble.score("cat"));
-// // console.log(word._word);
-//
-// console.log(scrabble.highestScore(["dog","a","catssss"]));
-
-// YOUR CODE HERE
-// Scrabble.prototype.helloWorld = function() {
-//   return 'hello world!';
-// };
 
 module.exports = Scrabble;
